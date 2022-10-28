2130cd4a83600d8864c4bde80a1cb8fde34719e2
Damien MIREMONT <damien.miremont@gmail.com>
2022-10-28 11:58:45 +0200

    Add popup for email send

--- src/main/java/fr/cda/disquesvyniles/disquesvyniles/VinyleApplication.java
+++ src/main/java/fr/cda/disquesvyniles/disquesvyniles/VinyleApplication.java
@@ -17,6 +17,7 @@ public class VinyleApplication extends Application {
         stage.show();
     }
 
+
     public static void main(String[] args) {
         launch();
     }
--- src/main/java/fr/cda/disquesvyniles/disquesvyniles/VinyleController.java
+++ src/main/java/fr/cda/disquesvyniles/disquesvyniles/VinyleController.java
@@ -1,7 +1,17 @@
 package fr.cda.disquesvyniles.disquesvyniles;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.text.MessageFormat;
 
 public class VinyleController {
     @FXML
@@ -43,19 +53,62 @@ public class VinyleController {
     @FXML
     private CheckBox checkboxCulturefactory;
 
+    @FXML
+    private TextArea champsResultat;
+
+    @FXML
+    private MenuItem EnvoiCourriel;
+
+
+
     public void effacerChamps() {
 
         champsTitre.setText("");
-       champsGenre.setValue(null);
+       champsGenre.setValue(null) ;
         champsDate.setValue(null);
         champsMin.setText("");
         champsMax.setText("");
+        champsResultat.setText("");
         checkboxDiscogs.setSelected(false);
         checkboxFnac.setSelected(false);
         checkboxVinylcorner.setSelected(false);
         checkboxLeboncoin.setSelected(false);
         checkboxMesvinyles.setSelected(false);
         checkboxCulturefactory.setSelected(false);
+    }
+
+    public void quitterFichier() {
+
+        Platform.exit();
+    }
+
+    public void onPopupClick() throws IOException {
+        PopupScene();
+    }
+
+    public void PopupScene() throws IOException {
+
+        Stage popupwindow=new Stage();
+
+        popupwindow.initModality(Modality.APPLICATION_MODAL);
+        popupwindow.setTitle("Envoi courriel");
+        Label label1= new Label("saisie du courriel");
+        Label label2= new Label("Veuillez saisir l'email de l'expéditeur");
+        TextField textField = new TextField();
+        Button button1= new Button("Envoyer");
+        Button button2= new Button("Annuler");
+        button1.setOnAction(e -> popupwindow.close());
+        button2.setOnAction(e -> popupwindow.close());
+        VBox layout= new VBox(10);
+        layout.getChildren().addAll(label1);
+        layout.getChildren().addAll(label2);
+        layout.getChildren().addAll(textField);
+        layout.getChildren().addAll(button1);
+        layout.getChildren().addAll(button2);
+        layout.setAlignment(Pos.CENTER);
+        Scene scene1= new Scene(layout, 300, 250);
+        popupwindow.setScene(scene1);
+        popupwindow.showAndWait();
 
     }
 }
